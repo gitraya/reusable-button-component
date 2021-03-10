ff56268d4d39d8b601960ead5a7ff6fedb355c7a
Raya <mail.raya15@gmail.com>
2021-03-10 23:22:12 +0700

    Add files via upload

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 import './Button.css';
 
-const Button = ({ color, text }) => {
-  const buttonClass = `button ft-ns-jp ${color}`;
+const Button = ({
+  color,
+  text,
+  variant,
+  disableShadow,
+  startIcon,
+  endIcon,
+}) => {
+  const buttonClass = `button ft-ns-jp ${color} ${variant} ${
+    disableShadow ? 'disableShadow' : ''
+  }`;
 
-  return <button className={buttonClass}>{text}</button>;
+  return (
+    <button className={buttonClass}>
+      {startIcon} {text} {endIcon}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: 'default',
+  text: 'Submit',
 };
 
 export default Button;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import { FaBeer } from 'react-icons/fa';
 import Button from './Button/Button';
 
 const Main = () => {
@@ -6,10 +7,16 @@ const Main = () => {
       <h2 className="ft-500 ft-poppins cl-gytwo">Buttons</h2>
       <div>
         <small>button</small>
-        <Button text="Submit" color="default" />
+        <Button text="Submit" color="default" disableShadow />
         <Button text="Submit" color="primary" />
         <Button text="Submit" color="secondary" />
-        <Button text="Submit" color="danger" />
+        <Button text="Submit" color="danger" variant="outline" />
+      </div>
+      <div>
+        <Button text="Submit" color="secondary" variant="text" />
+      </div>
+      <div>
+        <Button startIcon={<FaBeer />} />
       </div>
     </main>
   );
