5757a736ab36f0a09fdfc36878e21538234a8ec8
gitraya <mail.raya15@gmail.com>
2021-03-11 14:36:14 +0700

    Updates

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,28 +1,45 @@
-import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = ({
   color,
   text,
+  size,
   variant,
+  disabled,
   disableShadow,
   startIcon,
   endIcon,
+  demohover,
 }) => {
   const buttonClass = `button ft-ns-jp ${color} ${variant} ${
     disableShadow ? 'disableShadow' : ''
-  }`;
+  } ${demohover}`;
+
+  switch (size) {
+    case 'sm':
+      size = { padding: '6px 12px' };
+      break;
+    case 'md':
+      size = { padding: '8px 16px' };
+      break;
+    case 'lg':
+      size = { padding: '11px 22px' };
+      break;
+  }
 
   return (
-    <button className={buttonClass}>
-      {startIcon} {text} {endIcon}
+    <button className={buttonClass} style={size} disabled={disabled}>
+      <i className={startIcon ? 'startIcon' : ''}>{startIcon}</i>
+      {text}
+      <i className={endIcon ? 'endIcon' : ''}>{endIcon}</i>
     </button>
   );
 };
 
 Button.defaultProps = {
   color: 'default',
-  text: 'Submit',
+  text: 'Default',
+  size: 'md',
 };
 
 export default Button;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,116 @@
-import { FaBeer } from 'react-icons/fa';
 import Button from './Button/Button';
 
-const Main = () => {
+const Main = (text) => {
   return (
     <main>
       <h2 className="ft-500 ft-poppins cl-gytwo">Buttons</h2>
-      <div>
-        <small>button</small>
-        <Button text="Submit" color="default" disableShadow />
-        <Button text="Submit" color="primary" />
-        <Button text="Submit" color="secondary" />
-        <Button text="Submit" color="danger" variant="outline" />
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button />')}</small>
+          <Button />
+        </div>
+        <div className="item-demo">
+          <small className="cl-hover">{(text = '&:hover, &:focus')}</small>
+          <Button demohover="hovdef" />
+        </div>
       </div>
-      <div>
-        <Button text="Submit" color="secondary" variant="text" />
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button variant=”outline” />')}</small>
+          <Button variant="outline" color="primary" />
+        </div>
+        <div className="item-demo">
+          <small className="cl-hover">{(text = '&:hover, &:focus')}</small>
+          <Button variant="outline" color="primary" demohover="hovout" />
+        </div>
       </div>
-      <div>
-        <Button startIcon={<FaBeer />} />
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button variant=”text” />')}</small>
+          <Button variant="text" color="primary" />
+        </div>
+        <div className="item-demo">
+          <small className="cl-hover">{(text = '&:hover, &:focus')}</small>
+          <Button variant="text" color="primary" demohover="hovtex" />
+        </div>
+      </div>
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button disableShadow />')}</small>
+          <Button color="primary" disableShadow />
+        </div>
+      </div>
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button disabled />')}</small>
+          <Button disabled />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button varian="text" disabled />')}</small>
+          <Button variant="text" disabled />
+        </div>
+      </div>
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button startIcon=”local_grocery_store” />')}</small>
+          <Button
+            color="primary"
+            startIcon={<i class="fas fa-cart-plus"></i>}
+          />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button endIcon=”local_grocery_store” />')}</small>
+          <Button color="primary" endIcon={<i class="fas fa-cart-plus"></i>} />
+        </div>
+      </div>
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button size=”sm” />')}</small>
+          <Button color="primary" size="sm" />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button size=”md” />')}</small>
+          <Button color="primary" size="md" />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button size=”lg” />')}</small>
+          <Button color="primary" size="lg" />
+        </div>
+      </div>
+      <div className="container-btn-demo">
+        <div className="item-demo">
+          <small>{(text = '<Button color=”default” />')}</small>
+          <Button color="default" />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button color=”primary” />')}</small>
+          <Button color="primary" />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button color=”secondary” />')}</small>
+          <Button color="secondary" />
+        </div>
+        <div className="item-demo">
+          <small>{(text = '<Button color=”danger” />')}</small>
+          <Button color="danger" />
+        </div>
+      </div>
+      <div className="flex column">
+        <small className="cl-hover">{(text = '&:hover, &:focus')}</small>
+        <div className="container-btn-demo">
+          <div className="item-demo">
+            <Button color="default" demohover="hovdef" />
+          </div>
+          <div className="item-demo">
+            <Button color="primary" demohover="hovpri" />
+          </div>
+          <div className="item-demo">
+            <Button color="secondary" demohover="hovsec" />
+          </div>
+          <div className="item-demo">
+            <Button color="danger" demohover="hovdag" />
+          </div>
+        </div>
       </div>
     </main>
   );
